1eee3b40269e03dd592f9ef78ce5236c57470f3f
Bravo-Juan <147295289+Bravo-Juan@users.noreply.github.com>
2023-10-15 10:05:22 -0500

    Add files via upload

---
+++ module_3_problem_2.py
@@ -0,0 +1,10 @@
+# Juan Bravo
+# 10/15/23
+
+# Asks users for their name and greets them right after
+
+# Asks the user for their name
+user_name = input("Please enter your first and last name:")
+
+# Greets the user with their name
+print("Hello, " + user_name + ",", "it's nice to meet you!")
