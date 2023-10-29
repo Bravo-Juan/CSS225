a255d173823b41852dca358e6c1e5261a17406e2
Bravo-Juan <147295289+Bravo-Juan@users.noreply.github.com>
2023-10-29 16:48:26 -0500

    Add files via upload

---
+++ module_5_problem_1.py
@@ -0,0 +1,7 @@
+# Juan Bravo
+# 10/29/23
+
+# Prints "Hello World" to the screen 100 times
+
+for i in range(100):
+    print("Hello World")
---
+++ module_5_problem_2.py
@@ -0,0 +1,10 @@
+# Juan Bravo
+# 10/29/23
+
+# First loop prints each number in the list on a new line while the other loop prints each number and its square on a new line.
+
+for number in [12, 10, 32, 3, 66, 17, 42, 99, 20]:
+    print(number)
+
+for number in [12, 10, 32, 3, 66, 17, 42, 99, 20]:
+    print(number, "squared is", number ** 2)
